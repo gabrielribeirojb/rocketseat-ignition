75e2d1073c5c014ba8fb5f51d0241b33f4033603
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-12-10 20:10:36 -0300

    Listing repositories

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -4,12 +4,6 @@ import { useState, useEffect } from "react";
 
 // https://api.github.com/orgs/rocketseat/repos
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/unform/unform'
-}
-
 export function RepositoryList(){
 
     const [repositories, setRepositories] = useState([]);
@@ -25,10 +19,10 @@ export function RepositoryList(){
             <h1>Lista de Repositórios</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                })
+                }
             </ul>
         </section>
     );
